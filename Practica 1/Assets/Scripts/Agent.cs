9ce3b957d9dea72d6bf266e64a9c60356ac31e3a
PC <fernandounyen@gmail.com>
2019-09-09 21:18:05 -0500

    Hola

---
+++ Practica 1/Assets/Scripts/Agent.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Agent : MonoBehaviour
+{
+    // Agent
+    Vector3 velocity = Vector3.zero;
+    Vector3 desired = Vector3.zero;
+    Vector3 steer = Vector3.zero;
+
+    [SerializeField]
+    float maxSpeed = 1;
+    [SerializeField]
+    float maxSteer = 1;
+    
+    // ...
+    Transform target;
+    Transform area;
+
+    void Update()
+    {
+        target = GameObject.Find("Target").transform;
+        area = GameObject.Find("Area").transform;
+
+        // desired
+        desired = (target.position - transform.position).normalized * maxSpeed;
+        desired *= -1;
+        steer = Vector3.ClampMagnitude(desired - velocity, maxSteer);   
+
+        velocity += steer * Time.deltaTime;
+        transform.position += velocity * Time.deltaTime;  
+
+        if(transform.position.x <= -8 || transform.position.x >= 8 || transform.position.y >= 5.28 || transform.position.y <= -5.38){
+            Debug.Log("Has perdido");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);       
+        } 
+
+        float dist = Vector2.Distance(transform.position, area.position);
+
+        if(dist<=1){
+            Debug.Log("Has Ganado");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); 
+        }
+                
+    }
+}
---
+++ Practica 1/Assets/Scripts/Target.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{      
+ 
+    void Update()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.Scale(new Vector3(1, 1, 0));
+        transform.position = mousePosition;      
+    }
+}
