18fddf7569fe13e9047b42471bd2028ed2b45d36
Shuugo <fernandounyen@gmail.com>
2019-09-30 21:30:31 -0500

    Practica 2

---
+++ Practica 2/Assets/Scripts/Agent2.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Agent2 : MonoBehaviour
+{   
+    // Agent
+    Vector3 velocity = Vector3.zero;
+    Vector3 desired = Vector3.zero;
+    Vector3 steer = Vector3.zero;
+    Vector3 originPos = Vector3.zero;
+
+    public float AreaSquare = 2;
+
+    [SerializeField]
+    float maxSpeed = 1;
+    [SerializeField]
+    float maxSteer = 1;   
+    
+    // ...
+    Transform target;
+    
+    void Awake(){
+        originPos = transform.position;
+    }
+
+    void Update()
+    {
+        target = GameObject.Find("Target").transform;       
+
+        if(transform.position.x <= (originPos.x - AreaSquare) || transform.position.x >= (originPos.x + AreaSquare) ||
+        transform.position.y <= (originPos.y - AreaSquare) || transform.position.y >= (originPos.y + AreaSquare)){
+            velocity += Seek(originPos);
+            transform.position += velocity * Time.deltaTime;          
+        } else {
+            velocity += Seek(target.position, 100);
+        }
+            transform.position += velocity * Time.deltaTime;                     
+    }
+
+    public Vector3 Seek(Vector3 target, float range = 99999){ 
+        Vector3 desired = new Vector3(0, 0, 0);
+        Vector3 difference = (target - transform.position);                    
+        float distance = difference.magnitude;
+        desired = difference.normalized * maxSpeed;        
+        Vector3 steer;
+        if(distance< range){
+                steer = Vector3.ClampMagnitude(desired - velocity, maxSteer);
+        }else{
+                steer = Vector3.zero;
+        }     
+        return steer;        
+    }   
+}
