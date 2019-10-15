4ed5728da1ea6bb5eae0d1cc570066b0b5ded5d8
OMEGALUL <fernandounyen@gmail.com>
2019-10-14 21:50:35 -0500

    Parcial

---
+++ Fernando Unyen - Parcial/Assets/Agent.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Agent : SBAgent
+{	
+	public Transform currentTarget;
+	public char type;	
+
+	void Start()
+	{
+		maxSteer = 3f;
+		if(type == 'A'){
+			maxSpeed = 1f;
+				
+		} else if(type == 'B'){
+			maxSpeed = 3f;			
+		}
+	}
+
+	void Update()
+	{		
+		
+		velocity += SteeringBehaviours.Seek(this, currentTarget, 200);
+
+		if(type == 'A'){								
+			velocity += SteeringBehaviours.Separate(this, GameManager.agentsListB, 1f);
+		
+		}		
+		 if(type == 'B'){
+			velocity += SteeringBehaviours.Separate(this, GameManager.agentsListA, 1f);
+		}
+
+		transform.position += velocity * Time.deltaTime;
+		changeTarget();
+	}
+
+	public void changeTarget(){			
+		if(Vector3.Distance(this.transform.position, currentTarget.position) <= 0.1f && currentTarget.name != (GameObject.FindGameObjectsWithTag("Points").Length).ToString()){						
+			currentTarget = GameObject.Find((int.Parse(currentTarget.name) + 1).ToString()).transform;							
+								
+		} if(Vector3.Distance(this.transform.position, currentTarget.position) <= 0.1f && currentTarget.name == (GameObject.FindGameObjectsWithTag("Points").Length).ToString()){
+			Destroy(gameObject);
+			if(type == 'A'){
+				GameManager.agentsListA.Remove(this.gameObject);
+			}
+			if(type == 'B'){
+				GameManager.agentsListB.Remove(this.gameObject);
+			}
+
+		}				
+	}   
+}
---
+++ Fernando Unyen - Parcial/Assets/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+	public GameObject agentA;
+	public GameObject agentB;
+	public Vector3 spawner;
+	public static List<GameObject> agentsListA = new List<GameObject>();
+	public static List<GameObject> agentsListB = new List<GameObject>();
+	float n;
+
+	void Awake(){
+		agentA = Resources.Load<GameObject>("AgentA");
+		agentB = Resources.Load<GameObject>("AgentB");
+		spawner  = GameObject.Find("0").transform.position;
+	}
+	void Start(){			
+			StartCoroutine("SpawnA");
+			StartCoroutine("SpawnB");
+	
+	}
+
+	IEnumerator SpawnA() {  
+		GameObject instance = Instantiate(agentA); 
+		instance.GetComponent<Agent>().currentTarget = GameObject.Find("1").transform; 
+		agentsListA.Add(instance);
+        yield return new WaitForSeconds(2f);
+		StartCoroutine("SpawnA");
+	}	
+
+	IEnumerator SpawnB() {  
+		GameObject instance = Instantiate(agentB); 
+		instance.GetComponent<Agent>().currentTarget = GameObject.Find("1").transform;  
+		agentsListB.Add(instance);     
+        yield return new WaitForSeconds(4f);
+		StartCoroutine("SpawnB");
+	}		
+}
+
